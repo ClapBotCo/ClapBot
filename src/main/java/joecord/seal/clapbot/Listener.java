560cd44cf7ccf0e436606e5b7f415fcc3c3f9af7
Alec Laptop <alecadab@gmail.com>
2020-06-29 23:21:47 +0800

    Added the start of join message commands

--- src/main/java/joecord/seal/clapbot/Listener.java
+++ src/main/java/joecord/seal/clapbot/Listener.java
@@ -2,14 +2,15 @@ package joecord.seal.clapbot;
 
 import joecord.seal.clapbot.commands.CommandHandler;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class MessageListener extends ListenerAdapter
+public class Listener extends ListenerAdapter
 {
     private CommandHandler commandHandler;
 
-    public MessageListener(CommandHandler commandHandler) {
+    public Listener(CommandHandler commandHandler) {
         this.commandHandler = commandHandler;
     }
 
@@ -33,5 +34,11 @@ public class MessageListener extends ListenerAdapter
             System.out.println("Picture or empty message detected!");
             exception.printStackTrace();
         }
+    }
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        System.out.println(event.getUser().getName() + " has just joined " +
+            event.getGuild().getName());
     }
 }
