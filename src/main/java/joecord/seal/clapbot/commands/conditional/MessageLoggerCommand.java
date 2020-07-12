779c83f8459201de63076d5aaf27d113accf746a
Alec Laptop <alecadab@gmail.com>
2020-07-12 10:10:01 +0800

    Logging changes

--- src/main/java/joecord/seal/clapbot/commands/conditional/MessageLoggerCommand.java
+++ src/main/java/joecord/seal/clapbot/commands/conditional/MessageLoggerCommand.java
@@ -1,21 +1,27 @@
 package joecord.seal.clapbot.commands.conditional;
 
+import java.io.PrintStream;
+
 import joecord.seal.clapbot.api.CommandProperty;
 import joecord.seal.clapbot.api.GenericCommand;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
-public class LoggingCommand extends GenericCommand<MessageReceivedEvent> {
-    public LoggingCommand() {
+public class MessageLoggerCommand extends GenericCommand<MessageReceivedEvent> {
+    private PrintStream out;
+
+    public MessageLoggerCommand(PrintStream out) {
         super(MessageReceivedEvent.class, CommandProperty.RESPECT_BOTS);
 
         this.displayName = "Logging";
         this.description = "Logs MessageReceivedEvent details to System.out";
+
+        this.out = out;
     }
 
     @Override
     public void execute(MessageReceivedEvent event) {
-        System.out.println(String.format(
-            "#%s [%s]: %s\n",
+        this.out.println(String.format(
+            "#%s [%s]: %s",
             event.getTextChannel().getName(),
             event.getAuthor().getName(),
             event.getMessage().getContentRaw())
